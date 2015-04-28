f451175b764c91048440b0fcb17dc18b6592c364
unknown <benjamin@BLNB0001.vld.local>
2015-04-28 11:57:33 +0200

    very simple BinaryConverter

--- PicsiSWT/src/imageprocessing/BinaryConverter.java
+++ PicsiSWT/src/imageprocessing/BinaryConverter.java
@@ -3,6 +3,7 @@ package imageprocessing;
 import main.PicsiSWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.PaletteData;
 import org.eclipse.swt.graphics.RGB;
 import utils.Parallel;
 
@@ -18,26 +19,26 @@ public class BinaryConverter implements IImageProcessor {
     @Override
     public Image run(Image input, int imageType) {
         int threshold = 128;
-        ImageData inData = input.getImageData();
-        for (int v = 0; v < inData.height; v++) {
-            int failure = 0;
-            for (int u = 0; u < inData.width; u++) {
-                int pixel = inData.getPixel(u, v);
-                if (pixel + failure < threshold) {
-                    failure += pixel;
-                    pixel = 0;
-                } else {
-                    pixel = 255;
-                    failure -= 255;
-                    if (failure > 255) {
-                        failure = 0;
-                    }
-                }
-                inData.setPixel(u, v, pixel);
+
+        RGB[] colors = new RGB[2];
+        colors[0] = new RGB(0,0,0);
+        colors[1] = new RGB(255,255,255);
+        PaletteData paletteData = new PaletteData(colors);
+        int width = input.getImageData().width;
+        int height = input.getImageData().height;
+        byte[] oldData = input.getImageData().data;
+        ImageData imgData = new ImageData(width, height, 8, paletteData);
+        byte[] newData = imgData.data;
+        for (int i = 0; i < height * width; i++) {
+            int val = 0xFF & oldData[i];
+            if (val > threshold) {
+                newData[i] = 1;
+            } else {
+                newData[i] = 0;
             }
         }
 
 
-        return new Image(input.getDevice(), inData);
+        return new Image(input.getDevice(), imgData);
     }
 }
