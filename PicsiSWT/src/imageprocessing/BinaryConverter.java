d221e694f0ccddfc8007ad6ae1025bf7fcbe2661
unknown <benjamin@BLNB0001.vld.local>
2015-04-29 09:56:03 +0200

    correct binary image

--- PicsiSWT/src/imageprocessing/BinaryConverter.java
+++ PicsiSWT/src/imageprocessing/BinaryConverter.java
@@ -1,5 +1,6 @@
 package imageprocessing;
 
+import gui.MainWindow;
 import main.PicsiSWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
@@ -11,6 +12,7 @@ import utils.Parallel;
  * Created by benjamin on 22.04.2015.
  */
 public class BinaryConverter implements IImageProcessor {
+
     @Override
     public boolean isEnabled(int imageType) {
         return imageType == PicsiSWT.IMAGE_TYPE_GRAY;
@@ -18,6 +20,10 @@ public class BinaryConverter implements IImageProcessor {
 
     @Override
     public Image run(Image input, int imageType) {
+        byte[] mask = new byte[]{(byte) 0b10000000, 0b01000000, 0b00100000, 0b00010000, 0b00001000, 0b00000100, 0b00000010, 0b00000001};
+
+
+
         int threshold = 128;
 
         RGB[] colors = new RGB[2];
@@ -27,14 +33,18 @@ public class BinaryConverter implements IImageProcessor {
         int width = input.getImageData().width;
         int height = input.getImageData().height;
         byte[] oldData = input.getImageData().data;
-        ImageData imgData = new ImageData(width, height, 8, paletteData);
+        ImageData imgData = new ImageData(width, height, 1, paletteData);
         byte[] newData = imgData.data;
-        for (int i = 0; i < height * width; i++) {
+        int j = 0;
+        for (int i = 0; i < oldData.length; i++) {
             int val = 0xFF & oldData[i];
+
             if (val > threshold) {
-                newData[i] = 1;
-            } else {
-                newData[i] = 0;
+                newData[j] = (byte) (newData[j] | mask[i % 8]);
+
+            }
+            if (i%8 == 7) {
+                j++;
             }
         }
 
