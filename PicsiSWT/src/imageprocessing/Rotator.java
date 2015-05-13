d8998b4b5fa4d75ef812bedd9031c51856fb246a
Benjamin Leber <grisu118@grisu118.ch>
2015-05-13 14:30:24 +0200

    Added Rotator

--- PicsiSWT/src/imageprocessing/Rotator.java
+++ PicsiSWT/src/imageprocessing/Rotator.java
@@ -23,20 +23,23 @@ public class Rotator implements IImageProcessor {
         double cosa = Math.cos(alpha);
 
         ImageData data = (ImageData) input.getImageData().clone();
+        ImageData old = input.getImageData();
         int mx = data.width / 2;
         int my = data.height / 2;
-            Parallel.For(0, data.height, v -> {
-                for (int u = 0; u < data.width; u++) {
-                    int uu = (int) Math.floor((u - mx) * cosa + (v - my) * sina + 0.5) + mx;
-                    int vv = (int) Math.floor((u - mx) * sina * -1 + (v - my) * cosa + 0.5) + my;
-                    if (uu < 0 || uu >= data.width
-                            || vv < 0 || vv >= data.height) {
-                        data.setPixel(u, v, 0);
-                    } else {
-                        data.setPixel(u, v, input.getImageData().getPixel(uu, vv));
-                    }
+        int height = data.height;
+        int width = data.width;
+        Parallel.For(0, height, v -> {
+            for (int u = 0; u < width; u++) {
+                int uu = (int) Math.floor((u - mx) * cosa + (v - my) * sina + 0.5) + mx;
+                int vv = (int) Math.floor((u - mx) * sina * -1 + (v - my) * cosa + 0.5) + my;
+                if (uu < 0 || uu >= width
+                        || vv < 0 || vv >= height) {
+                    data.setPixel(u, v, 0);
+                } else {
+                    data.setPixel(u, v, old.getPixel(uu, vv));
                 }
-            });
+            }
+        });
         return new Image(input.getDevice(), data);
     }
 }
