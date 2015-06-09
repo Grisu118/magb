b2e79885db974f088ceb19600def5236486fc8a0
Grisu118 <grisu118@grisu118.ch>
2015-06-09 20:55:27 +0200

    finished Scale

--- PicsiSWT/src/imageprocessing/Scale.java
+++ PicsiSWT/src/imageprocessing/Scale.java
@@ -28,58 +28,54 @@ public class Scale implements IImageProcessor {
 
         int mx = outData.width/2;
         int my = outData.height/2;
-
-        Matrix matrix = Matrix.translation(mx, my)
-                .multiply(Matrix.scaling(scale, scale))
-                .multiply(Matrix.translation(-mx, -my))
-                .inverse();
-
-        int offset = 0;
-        //Parallel.For(0, outData.height, v -> {
-        try {
-            for (int v = 0; v < outData.height; v++) {
-                double[] v1 = {0, v, 1};
-
+        Parallel.For(0, outData.height, v -> {
                 for (int u = 0; u < outData.width; u++) {
-                    v1[0] = u;
-                    double[] v2 = matrix.multiply(v1);
-                    int x = (int) (scale * u);
-                    int y = (int) (scale * v);
-                    double x_diff = (scale * v) - x;
-                    double y_diff = (scale * u) - y;
+
+                    double x = u/scale;
+                    double y = v/scale;
+
                     if (x < 0 || x >= outData.width - 1
                             || y < 0 || y >= outData.height - 1) {
                         outData.setPixel(u, v, 0);
                         outData.setAlpha(u, v, 0);
                     } else {
+                        int uA = (int)x;
+                        int vA = (int)y;
+                        int uB = uA + 1;
+                        if (uB >= inData.width) {
+                            uB = uA;
+                        }
+                        int vC = vA + 1;
+                        if (vC >= inData.height) {
+                            vC = vA;
+                        }
+                        int uD = uB;
+                        int vD = vC;
 
-                        RGB A = inData.palette.getRGB(inData.getPixel(x, y));
-                        int Aa = inData.getAlpha(x, y);
-                        RGB B = inData.palette.getRGB(inData.getPixel(x + 1, y));
-                        int Ba = inData.getAlpha(x + 1, y);
-                        RGB C = inData.palette.getRGB(inData.getPixel(x, y + 1));
-                        int Ca = inData.getAlpha(x, y + 1);
-                        RGB D = inData.palette.getRGB(inData.getPixel(x + 1, y + 1));
-                        int Da = inData.getAlpha(x + 1, y + 1);
-                        double Er = A.red + x_diff * (B.red - A.red);
-                        double Gr = Er + y_diff * ((C.red + x_diff * (D.red - C.red)) - Er);
-                        double Eg = A.green + x_diff * (B.green - A.green);
-                        double Gg = Eg + y_diff * ((C.green + x_diff * (D.green - C.green)) - Eg);
-                        double Eb = A.blue + x_diff * (B.blue - A.blue);
-                        double Gb = Eb + y_diff * ((C.blue + x_diff * (D.blue - C.blue)) - Eb);
-                        double Ea = Aa + x_diff * (Ba - Aa);
-                        double Fa = Ca + x_diff * (Da - Ca);
-                        double Ga = Ea + y_diff * (Fa - Ea);
-                        RGB rgb = new RGB(ImageProcessing.clamp((int) Gr), ImageProcessing.clamp((int) Gg), ImageProcessing.clamp((int) Gb));
-                        int pix = outData.palette.getPixel(rgb);
-                        outData.setPixel(u, v, pix);
-                        outData.setAlpha(u, v, (int) Ga);
+                        double a = x-uA;
+                        double b = y-vA;
+
+                        RGB A = inData.palette.getRGB(inData.getPixel(uA, vA));
+                        int aA = inData.getAlpha(uA, vA);
+                        RGB B = inData.palette.getRGB(inData.getPixel(uB, vA));
+                        int aB = inData.getAlpha(uB, vA);
+                        RGB C = inData.palette.getRGB(inData.getPixel(uA, vC));
+                        int aC = inData.getAlpha(uA, vA);
+                        RGB D = inData.palette.getRGB(inData.getPixel(uD, vD));
+                        int aD = inData.getAlpha(uD, vD);
+
+                        RGB E = new RGB((int)((A.red + a * (B.red - A.red))),(int)((A.green + a * (B.green - A.green))),(int)((A.blue + a * (B.blue - A.blue))));
+                        int aE = (int) (aA + a * (aB - aA));
+                        RGB F = new RGB((int)((C.red + a * (D.red - C.red))),(int)((C.green + a * (D.green - C.green))),(int)((C.blue + a * (D.blue - C.blue))));
+                        int aF = (int) (aC + a* (aD - aC));
+                        RGB G = new RGB((int)((E.red + b * (F.red - E.red))),(int)((E.green + b * (F.green - E.green))),(int)((E.blue + b * (F.blue - E.blue))));
+                        int aG = (int) (aE + b * (aF - aE));
+
+                        outData.setPixel(u, v, outData.palette.getPixel(G));
+                        outData.setAlpha(u, v, aG);
                     }
                 }
-            }//);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            });
 
         return new Image(input.getDevice(), outData);
     }
